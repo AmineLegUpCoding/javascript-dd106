73ac95ca32b0dd3fe379c584726ad2db8e4ade8a
Amine legdani <legdani.amine@gmail.com>
2024-01-30 08:41:23 +0000

    test git

---
+++ session-02/index.js
@@ -0,0 +1,32 @@
+// Les fonctions en Javascript
+function greet(){
+    //console.log("Hellooo! " + nom)
+}
+
+function somme(nombre1, nombre2){
+    return nombre1 + nombre2
+}
+
+// déclarations des variables
+const PI = 3.14
+function inscrire(){
+    let nom;
+    let prenom;
+    let age;
+    nom = "KESSOU"
+    prenom = "Ahmed"
+    age=21   
+    if (age < 22){
+        var statut;
+        statut = "stagiaire"
+    } 
+    console.log(nom)
+    console.log(statut)
+
+}
+
+inscrire()
+
+
+
+
---
+++ session-03/index.js
@@ -0,0 +1,36 @@
+let a = 5;
+let b = "5";
+
+if (a === b){
+    //console.log("Les deux valeurs sont égales!")
+}else {
+    //console.log("Les deux valeurs sont différents!")
+};
+
+
+let statut ="étudiant";
+let age = 20;
+
+if (statut == "étudiant" || 
+
+
+
+=== 20 ){
+    console.log("okkk")
+}else if (statut == "employé"){
+    console.log("error")
+}
+
+
+let valide = false;
+
+if (!valide ){ // false = false
+    console.log("ce stagiaire n'est pas insctrit")
+}
+
+
+let test;
+
+if (!test){//undefined
+    console.log("okkkk undefined")
+}
